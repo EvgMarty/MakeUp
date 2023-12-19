144a26702429fbe4e7095d2497ca474dd59f54ef
EvgMarty <mrtevgeniy@gmail.com>
2023-12-19 13:21:15 +0200

    motion

--- src/components/SliderComponent/SliderComponent.jsx
+++ src/components/SliderComponent/SliderComponent.jsx
@@ -1,15 +1,19 @@
 import styles from './SliderComponent.module.scss';
 import React, { useState } from 'react';
 import Slider from 'react-slick';
+import { motion, AnimatePresence } from 'framer-motion';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 
 const SliderComponent = () => {
+  const [currentSlide, setCurrentSlide] = useState(0);
+
   const settings = {
     autoplay: true,
-    autoplaySpeed: 4000,
+    autoplaySpeed: 5000,
     arrows: false,
     dots: false,
+    afterChange: (index) => setCurrentSlide(index),
   };
 
   const images = [
@@ -45,11 +49,20 @@ const SliderComponent = () => {
           <div key={index} className={styles.slideItem}>
             <img src={image} alt={`Slide ${index + 1}`} />
 
-            <div className={styles.overlayText}>
-              <p>{slideContents[index].title}</p>
-              <h3>{slideContents[index].description}</h3>
-              <h5>{slideContents[index].descrip}</h5>
-            </div>
+            <AnimatePresence>
+              {currentSlide === index && (
+                <motion.div
+                  initial={{ opacity: 0, x: -100 }}
+                  animate={{ opacity: 1, x: 0 }}
+                  exit={{ opacity: 0, x: -100 }}
+                  className={styles.overlayText}
+                >
+                  <p>{slideContents[index].title}</p>
+                  <h3>{slideContents[index].description}</h3>
+                  <h5>{slideContents[index].descrip}</h5>
+                </motion.div>
+              )}
+            </AnimatePresence>
           </div>
         ))}
       </Slider>
